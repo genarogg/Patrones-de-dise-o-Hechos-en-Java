1c46be9ee709edd1039f25d120b694c320957671
Genarogg <gena_gonzalez15@hotmail.com>
2019-07-09 22:01:27 -0400

    funcion  del patron factory

--- src/Patrones1Creacionales/AbstractFactory1/Main.java
+++ src/Patrones1Creacionales/AbstractFactory1/Main.java
@@ -4,6 +4,11 @@ import java.util.Scanner;
 
 /**
  * @author Genarogg
+ * 
+ * El patrón Abstract Factory nos permite crear, mediante una interfaz, 
+ * conjuntos o familias de objetos (denominados productos) que dependen
+ * mutuamuente y todo esto sin especificar cual es el objeto concreto.
+ *
  */
 
 public class Main{
