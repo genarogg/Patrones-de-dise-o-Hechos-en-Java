a95e58f71fde361fdf3f4969bc19971e785dca8a
Genarogg <gena_gonzalez15@hotmail.com>
2019-07-13 00:50:21 -0400

    Mas Clases del patron Facade

---
+++ src/Patrones2Estructurales/Facade/B.java
@@ -0,0 +1,10 @@
+package Patrones2Estructurales.Facade;
+
+/**
+ * @author Genarogg
+ */
+public class B {
+    public void ConoserColor(){
+        System.out.println("Busco si es B/N o color");
+    }
+}
---
+++ src/Patrones2Estructurales/Facade/C.java
@@ -0,0 +1,13 @@
+package Patrones2Estructurales.Facade;
+
+/**
+ * @author Genarogg
+ */
+public class C {
+    public void CargarHoja(){
+        System.out.println("Cargo la hoja");
+    }
+    public void imprimir(){
+        System.out.println("imprimir");
+    }
+}
