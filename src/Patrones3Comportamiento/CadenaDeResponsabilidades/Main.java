27bcbb132bc2d3b325d69ec637cd18690f0ee460
Genarogg <gena_gonzalez15@hotmail.com>
2019-07-13 01:21:49 -0400

    Fin del patron Cadena de responsabilidades

--- src/Patrones3Comportamiento/CadenaDeResponsabilidades/Main.java
+++ src/Patrones3Comportamiento/CadenaDeResponsabilidades/Main.java
@@ -4,6 +4,13 @@ import javax.swing.JOptionPane;
 
 /**
  * @author Genarogg
+ * 
+ * El patrón de diseño Chain of Responsibility es un patrón 
+ * de comportamiento que evita acoplar el emisor de una petición 
+ * a su receptor dando a más de un objeto la posibilidad de 
+ * responder a una petición. Para ello, se encadenan los receptores 
+ * y pasa la petición a través de la cadena hasta que es procesada 
+ * por algún objeto
  */
 public class Main {
     public static void main(String[] args) {
@@ -14,6 +21,7 @@ public class Main {
         a.setSucesor(c);
         c.setSucesor(g);
         
+        //Resibe un valor numerico para autirizar un prestamo  ejemplo 40 000
         int valor = Integer.parseInt(JOptionPane.showInputDialog
                     (null, "Introduce el valor del Prestamo", "Prestamo"));
         
