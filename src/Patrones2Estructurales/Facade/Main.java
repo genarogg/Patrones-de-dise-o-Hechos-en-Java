52c5483e0d1b52e5840d23a8591cd7093d079e90
Genarogg <gena_gonzalez15@hotmail.com>
2019-07-13 00:54:28 -0400

    Clase main del patron facade

--- src/Patrones2Estructurales/Facade/Main.java
+++ src/Patrones2Estructurales/Facade/Main.java
@@ -1,14 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Patrones2Estructurales.Facade;
 
 /**
- *
  * @author Genarogg
  */
 public class Main {
+    public static void main(String[] args) {
+        new Main();
+    }
     
+    public Main(){
+        Facade f = new Facade();
+        f.imprime();
+    }
 }
