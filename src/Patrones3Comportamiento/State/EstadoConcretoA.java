f8f8c9fb3b056cfe04c413eeeef599aec0684052
Genarogg <gena_gonzalez15@hotmail.com>
2019-07-14 19:31:42 -0400

    +Clase de state

---
+++ src/Patrones3Comportamiento/State/EstadoConcretoA.java
@@ -0,0 +1,12 @@
+package Patrones3Comportamiento.State;
+
+/**
+ * @author Genarogg
+ */
+public class EstadoConcretoA extends Estado{
+
+    @Override
+    public void mostrar() {
+        System.out.println("Estado A");
+    } 
+}
